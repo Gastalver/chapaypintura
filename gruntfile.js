01ebdd31d60acbb44f70dd9c626b799587c7c4da
Gastalver <mgastalver@gmail.com>
2015-01-25 17:39:18 +0100

    Añadido archivo gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt)
+{
+
+grunt.registerTask('despliega','Crea la estructura de directorios de trabajo',function(){
+    console.log('desplegando...');
+});
+ 
+ 
+ 
+};
