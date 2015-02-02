ef2db42bf2c4cb30e52a3d7611bf475c2fd77354
Miguel Gastalver <mgastalver@gmail.com>
2015-02-03 00:41:48 +0100

    Anadidos modulos y tareas.

--- gruntfile.js
+++ gruntfile.js
@@ -1,10 +1,29 @@
-module.exports = function(grunt)
-{
+module.exports = function (grunt) {
+  // Carga todas las tareas a partir de los nodos que incluyan la palabra grunt.
+  require('load-grunt-tasks')(grunt);
+  // Muestra el tiempo que tarda cada tarea.
+  require('time-grunt')(grunt);
 
-grunt.registerTask('despliega','Crea la estructura de directorios de trabajo',function(){
-    console.log('desplegando...');
+  
+
+// Crea los directorios y archivos de trabajo.
+grunt.registerTask('carpetas', function() {
+	
+	var fs=require('fs');
+	fs.mkdir('./app');
+	fs.mkdir('./app/fonts');
+	fs.mkdir('./app/less');
+	fs.mkdir('./app/styles');
+	fs.mkdir('./app/scripts');
+	console.log('Carpetas creadas');
+	
+	
+	fs.writeFile('./app/less/main.less', 'cojones', function (err) {
+       if (err) throw err;
+       console.log('It\'s saved!');
+    });
+	
 });
- 
- 
- 
+
+  
 };
