c8486c62b07ba889b8b7fd4819fd63cf62e86cf2
Miguel Gastalver <mgastalver@gmail.com>
2015-02-07 12:31:05 +0100

    Prueba con lib. Borrar

--- gruntfile.js
+++ gruntfile.js
@@ -3,27 +3,48 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   // Muestra el tiempo que tarda cada tarea.
   require('time-grunt')(grunt);
+  grunt.loadNpmTasks("grunt-bower-install-simple");
 
+  // Configurasione
+  grunt.initConfig({
+    "bower-install-simple": {
+        options: {
+            color: true,
+			cwd: "app",
+			directory: "lib"
+        }
+		,
+        "prod": {
+            options: {
+                production: true
+            }
+        },
+        "dev": {
+            options: {
+                production: false
+            }
+        }
+    }
+ 
+   });
   
-
-// Crea los directorios y archivos de trabajo.
+   
+  
+// Crea los directorios y archivos de trabajo que faltan.
 grunt.registerTask('carpetas', function() {
 	
 	var fs=require('fs');
-	fs.mkdir('./app');
 	fs.mkdir('./app/fonts');
-	fs.mkdir('./app/less');
 	fs.mkdir('./app/styles');
 	fs.mkdir('./app/scripts');
 	console.log('Carpetas creadas');
-	
-	
-	fs.writeFile('./app/less/main.less', 'cojones', function (err) {
-       if (err) throw err;
-       console.log('It\'s saved!');
-    });
-	
 });
+
+// Instala los componentes Bower. Basicamente Bootstrap y JQuery.
+grunt.registerTask("bower-install", [ "bower-install-simple:dev" ]);
+
+grunt.registerTask("inicio", ["carpetas"],[ "bower-install-simple" ]);
+
 
   
 };
