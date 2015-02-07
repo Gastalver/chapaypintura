87f35b416464cce08495f458be68930fb54a6f59
Miguel Gastalver <mgastalver@gmail.com>
2015-02-07 18:26:58 +0100

    Cambios

--- gruntfile.js
+++ gruntfile.js
@@ -23,17 +23,39 @@ module.exports = function (grunt) {
             }
         }
 	},
-	"clean": {
+	clean: {
         app: ["app/styles", "app/bower_components", "app/scripts", "app/fonts"],
         todo: ["./app"]
+    },
+	less: {
+      dist: {
+        files: {
+          'app/styles/main.css': ['app/less/main.less']
+        },
+        options: {
+          sourceMap: true,
+          sourceMapFilename: 'app/styles/main.css.map',
+          sourceMapBasepath: 'app',
+          sourceMapRootpath: '/'
         }
+      }
+    },
+	watch: {
+		less: {
+			files:['app/less/**/*.less'],
+			tasks:'cssiza',
+			options:{
+				livereload: true
+			}
+		}	
+	}
    });
 
 // Borra archivos y carpetas
-grunt.registerTask('limpia', ["clean:app"]);
+grunt.registerTask('limpieza', 'Borra archivos y carpetas regenerables.',["clean:app"]);
    
 // Crea los directorios y archivos de trabajo que faltan.
-grunt.registerTask('carpetiza', function() {
+grunt.registerTask('carpetiza', 'Crea todas las carpetas necesarias.',function() {
 	
 	var fs=require('fs');
 	fs.mkdir('./app/fonts');
@@ -43,9 +65,14 @@ grunt.registerTask('carpetiza', function() {
 });
 
 // Instala las fuentes de los componentes del front-end  con Bower: Bootstrap, JQuery y FontAwesome.
-grunt.registerTask("boweriza", [ "bower-install-simple:dev" ]);
-grunt.registerTask('inicio', ['carpetas','bower-install-simple:dev']);
+grunt.registerTask('boweriza','Instala todos los componentes del front-end via Bower.', [ "bower-install-simple:dev" ]);
+grunt.registerTask('inicializa', 'Despliega el entorno. Es el primer comando a ejecutar.',['carpetiza','boweriza']);
+
+// Transcompila de less a css
+grunt.registerTask('cssiza', ["less:dist"]);
+
+//Vigila si hay cambios   
+grunt.registerTask("vigila",'Vigila si hay cambios en los archivos less, recompila y recarga.',['watch']);
 
 
-  
 };
