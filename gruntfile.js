541c728ed45ead07dde1683b347e3c1fea7bfc9a
Miguel Gastalver <mgastalver@gmail.com>
2015-02-07 13:49:28 +0100

    Eliminada carpeta lib

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,10 @@
 module.exports = function (grunt) {
   // Carga todas las tareas a partir de los nodos que incluyan la palabra grunt.
   require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt, {pattern: ['grunt-contrib-*', 'grunt-shell', 'grunt-bower-*']});
   // Muestra el tiempo que tarda cada tarea.
   require('time-grunt')(grunt);
-  grunt.loadNpmTasks("grunt-bower-install-simple");
+  //grunt.loadNpmTasks("grunt-bower-install-simple");
 
   // Configurasione
   grunt.initConfig({
@@ -11,8 +12,7 @@ module.exports = function (grunt) {
         options: {
             color: true,
 			cwd: "app",
-			directory: "lib"
-        }
+		}
 		,
         "prod": {
             options: {
@@ -43,7 +43,12 @@ grunt.registerTask('carpetas', function() {
 // Instala los componentes Bower. Basicamente Bootstrap y JQuery.
 grunt.registerTask("bower-install", [ "bower-install-simple:dev" ]);
 
-grunt.registerTask("inicio", ["carpetas"],[ "bower-install-simple" ]);
+grunt.registerTask("inicio", ["carpetas", "bower-install-simple:dev" ]);
+
+grunt.registerTask('escribe', function(){
+	//echo "@import \"../bower_components/bootstrap/less/bootstrap.less\";" > main2.less;
+	console.log("@import \"../bower_components/bootstrap/less/bootstrap.less\";" > borrar.txt) ;
+});
 
 
   
